275971dcc72cd408ff9f39cff17cfc1659c9e75d
mdashlw <mdashlw@gmail.com>
2019-05-14 14:52:37 +0300

    2.0.2
    * Update okhttp timeouts to 15 seconds

--- src/main/kotlin/ru/mdashlw/hypixel/api/ranked/HypixelRankedAPI.kt
+++ src/main/kotlin/ru/mdashlw/hypixel/api/ranked/HypixelRankedAPI.kt
@@ -10,13 +10,19 @@ import ru.mdashlw.hypixel.api.ranked.reply.Reply
 import ru.mdashlw.hypixel.api.ranked.reply.impl.LeaderboardReply
 import ru.mdashlw.hypixel.api.ranked.reply.impl.PlayerReply
 import ru.mdashlw.hypixel.api.ranked.util.newCall
+import java.util.concurrent.TimeUnit
 import kotlin.reflect.KClass
 
 object HypixelRankedAPI {
     private const val BASE_URL = "https://hypixel-ranked-api.herokuapp.com/"
 
     private val jackson: ObjectMapper = jacksonObjectMapper()
-    private val okHttpClient: OkHttpClient = OkHttpClient()
+    private val okHttpClient: OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .build()
 
     fun getPlayerByName(name: String): Player? =
         get(PlayerReply::class, "player/$name")
