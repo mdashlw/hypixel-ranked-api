4027c76085f3c61d45f4068d5913d8f3ced07e24
Davyd Malikov <mdashlw@gmail.com>
2019-05-31 10:28:50 +0300

    3.0.2
    * Fix Leaderboard

--- src/main/kotlin/ru/mdashlw/hypixel/api/ranked/entities/Leaderboard.kt
+++ src/main/kotlin/ru/mdashlw/hypixel/api/ranked/entities/Leaderboard.kt
@@ -1,6 +1,6 @@
 package ru.mdashlw.hypixel.api.ranked.entities
 
-class Leaderboard : ArrayList<Player>() {
+class Leaderboard : ArrayList<Leaderboard.Player>() {
     data class Player(
         val name: String,
         val rating: Int,
